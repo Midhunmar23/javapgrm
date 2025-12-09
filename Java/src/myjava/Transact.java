91efb5470b99a90b3ec382bfbc1f4233811098c3
MilMid <midhun.kumar@ibilglobal.com>
2025-12-09 14:27:20 +0530

    commit

---
+++ Java/src/myjava/Transact.java
@@ -0,0 +1,20 @@
+package myjava;
+
+public class Transact extends Inher
+{
+	
+		private String Customer = "James";
+		
+		public static void main (String args [])
+		{
+			Transact Num= new Transact();
+			
+			Num.acchold();
+			
+			System.out.println(Num.Impsnum +" " + Num.Customer );
+		}
+		
+		
+	}
+
+
