52ac19ea60c74526e8cd1272bf0cce4a72c083de
MilMid <midhun.kumar@ibilglobal.com>
2025-12-09 14:24:39 +0530

    Done

---
+++ Java/src/myjava/Stringprint.java
@@ -0,0 +1,31 @@
+package myjava;
+
+public class Stringprint 
+{
+	
+	
+	
+	public static void main(String args[])
+	{
+		int age=15;
+		
+		switch(age)
+		{
+		  
+		case 1:
+			System.out.println("Single");
+			
+		case 2:
+			System.out.println("2");
+			
+		case 15:
+			
+			System.out.println("10th");
+		}
+		
+		
+			
+		
+	}
+
+}
